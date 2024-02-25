a9e76a4f971411073dcbceeb278f4a80dd7a4147
MrKalyanNick <kalyanithinb@gmail.com>
2024-02-25 14:41:41 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 //https://vitejs.dev/config/
-base: "/Mate-Tube"
+base: "https://mrkalyanking.github.io/Mate-Tube/"
 
 export default defineConfig({
   plugins: [react()],
